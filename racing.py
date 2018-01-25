649862fe6a287bb051fef0b2c6fa933b92b3749b
PJ <pj.prins@codingwithkids.com>
2018-01-25 11:51:03 -0800

    Obstacles added

--- racing.py
+++ racing.py
@@ -3,28 +3,36 @@ from pygame.locals import *
 from racing_classes import *
 
 pygame.init()
-color = (26, 255, 255)
 size = (width, height) = (850, 480)
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
-speed = 5
+speed = 1
 
 def main():
+    global speed
     road = []
     road.append(Road(screen,"images/road1.png",0))
     road.append(Road(screen,"images/road2.png",-480))
     car = RaceCar(screen,"images/car.png")
+    obstacles = []
+    for i in range(4):
+        obstacles.append(Obstacle(screen, "images/cone.png", 150 + i * 50, -280))
+        obstacles.append(Obstacle(screen,"images/cone.png",360+i*50, -30))
     while True:
         clock.tick(60)
         for event in pygame.event.get():
             if event.type == QUIT:
                 sys.exit()
-        for r in road:
-            r.update(10)
 
-        screen.fill(color)
         for r in road:
+            r.update(speed)
             r.draw()
+
+        for o in obstacles:
+            if(o.update(speed)):
+                pass
+            else:
+                o.draw()
         car.draw()
         pygame.display.flip()
 
--- racing_classes.py
+++ racing_classes.py
@@ -1,3 +1,5 @@
+import random
+
 import pygame
 from pygame.locals import *
 
@@ -26,8 +28,28 @@ class RaceCar:
         self.img = pygame.image.load(img_path)
         self.img = pygame.transform.scale(self.img, (75, 150))
         self.rect = self.img.get_rect()
-        self.rect.move_ip(290,320)
+        self.rect.move_ip(290,250)
         self.screen = screen
 
     def draw(self):
         self.screen.blit(self.img,self.rect)
+
+class Obstacle:
+
+    def __init__(self,screen,img_path,x,y):
+        self.img = pygame.image.load(img_path)
+        self.img = pygame.transform.scale(self.img, (50, 50))
+        self.rect = self.img.get_rect()
+        self.rect.move_ip(x, y)
+        self.screen = screen
+
+    def update(self,speed):
+        self.rect.move_ip([0,speed])
+        if(self.rect.top > self.screen.get_height()):
+            self.rect.move_ip([0,-550])
+            return True
+        return False
+
+
+    def draw(self):
+        self.screen.blit(self.img, self.rect)
