d0d06f9cde755c6eb522ae5267214056c99f4555
PJ Prins <pj.prins@codingwithkids.com>
2018-01-31 13:52:59 -0800

    Added functionality to the bouncing ball program

--- BouncingBall/ball.py
+++ BouncingBall/ball.py
@@ -1,23 +1,22 @@
-import pygame
+import pygame, random
 from pygame.locals import *
 
 class Ball:
-    def __init__(self,screen):
+    def __init__(self,screen, x, y):
         self.img = pygame.image.load("../images/ball.png")
-        self.img = pygame.transform.scale(self.img,(20,20))
+        scale = random.randint(1,5)*10
+        self.img = pygame.transform.scale(self.img,(scale, scale))
         self.rect = self.img.get_rect()
-        self.rect.move_ip(320, 200)
-        self.speed = [2, -2]
+        self.rect.move_ip(x, y)
+        self.speed = [random.randint(-5, 5), random.randint(-5, 5)]
         self.screen = screen
 
-    def move(self,width,height):
+    def move(self, width, height):
         self.rect.move_ip(self.speed)
         if self.rect.left < 0 or self.rect.right > width:
             self.speed[0] *= -1
-            self.rect.move_ip(self.speed)
         elif self.rect.top < 0 or self.rect.bottom > height:
             self.speed[1] *= -1
-            self.rect.move_ip(self.speed)
 
     def draw(self):
         self.screen.blit(self.img,self.rect)
--- BouncingBall/bounce.py
+++ BouncingBall/bounce.py
@@ -8,17 +8,28 @@ screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 
 color = (26, 255, 255)
+balls = []
 
 def main():
-    ball = Ball(screen)
+    for i in range(10):
+        balls.append(Ball(screen, width/2,height/2))
     while True:
         clock.tick(60)
         for event in pygame.event.get():
             if event.type == QUIT:
                 sys.exit()
-        ball.move(width,height)
+            if event.type == MOUSEBUTTONDOWN:
+                balls.append(Ball(screen, event.pos[0], event.pos[1]))
+            if event.type == KEYDOWN:
+                if event.key == K_d:
+                    for i in range(len(balls)//2):
+                        balls.pop(0)
+
         screen.fill(color)
-        ball.draw()
+        for ball in balls:
+            ball.move(width,height)
+        for ball in balls:
+            ball.draw()
         pygame.display.flip()
 
 if __name__ == '__main__':
